2d263d74c3dd073889e9e98afde16c6f0a42843e
Eyad Arnabeh <eyadof@gmail.com>
2014-11-19 17:08:47 +0200

    replace socket connection via rmi calles

--- SearchEngean/src/searchengean/PServer.java
+++ SearchEngean/src/searchengean/PServer.java
@@ -13,18 +13,24 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import multicast.mcsend;
+import java.util.ArrayList;
 
 /**
  *
  * @author CG
  */
-public class PServer extends UnicastRemoteObject implements PServerInterface{
+public class PServer extends UnicastRemoteObject implements PServerInterface, SServerinterface{
+    //multicast channel variable
     private mcsend channel;
+    //secondary servers number increased on join
+    private int SServers = 0;
+    //array list of response from secondary servers
+    private ArrayList<String> response = new ArrayList<>();
+    
     public PServer () throws RemoteException, Exception {
         super() ;
-        //TODO:open a tcp socket server to recieve from secondary servers
         //open a multicast socket to connect with secondary servers
-        this.channel = new mcsend(5000, "228.5.6.7", 1);
+        this.channel = new mcsend(5001, "228.5.6.7", 1);
     }
     
     @Override
@@ -38,11 +44,26 @@ public class PServer extends UnicastRemoteObject implements PServerInterface{
         }
         if(songSearch.equals("Song Not Found")){
             try {
+                //multicast all secondary servers the song's name
                 this.channel.send(songName);
+                //wait for responses from the secondary servers
+                boolean SecondarySearchDone = false;
+                while(!SecondarySearchDone){
+                    if(this.SServers == response.size())
+                        SecondarySearchDone = true;
+                }
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
         return songSearch ;
+    }
+    @Override
+    public void join(){
+        this.SServers += 1 ; 
+    }
+    @Override
+    public void submitResault(String resault){
+        this.response.add(resault);
     }
 }
--- SearchEngean/src/searchengean/SServer.java
+++ SearchEngean/src/searchengean/SServer.java
@@ -9,32 +9,36 @@ import InputOutFiles.ReadFromFile;
 import multicast.mcjoin;
 import java.io.*;
 import java.net.Socket;
-
+import java.rmi.registry.*;
 /**
  *
  * @author eyadof
  */
 public class SServer {
+    
     public static void main (String[] args) throws Exception{
-        //open a tcp socket connection
-        Socket tcpSocket = new Socket("localhost", 6789);
-	DataOutputStream outToServer = new DataOutputStream(tcpSocket.getOutputStream());
-        
+        //define registry port
+        int rport = 5000;
+        Registry registry = LocateRegistry.getRegistry("localhost",rport);
+        //get sserver interface object
+        SServerinterface pserver = (SServerinterface) registry.lookup("//localhost:5000/SearchEngean");
         //define multicast group and port
-        int port = 5000;
+        int port = 5001;
         String group = "228.5.6.7";
         //join the group 
         mcjoin channel = new mcjoin(port,group);
+        //send join signal to the primary server
+        pserver.join();
         //waiting for reciving a new message
         while(true){
             String message = channel.receive();
             //search for the song
             String song = ReadFromFile.readFromFile(message, "songs.txt");
             if(song.equals("Song Not Found")){
-                outToServer.writeBytes("Song Not Found");
+                pserver.submitResault("Song Not Found");
             }
             else
-                outToServer.writeBytes(song);
+                pserver.submitResault(song);
         }
         
     }
